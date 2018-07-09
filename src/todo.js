1e99d1fe7abbd1943c32bd44079fc73874646449
sachin <sachinjangid021@gmail.com>
2018-07-09 22:13:20 +0530

    function to display list items

--- src/todo.js
+++ src/todo.js
@@ -2,10 +2,45 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
 class Todo extends Component {
+  constructor() {
+    super()
+    this.state = {
+      items: [],
+    }
+    this.addTask = this.addTask.bind(this)
+  }
+
+  listItems(items) {
+    return items.map(item => {
+      return (
+        <div id={item.id}>
+          <h4>{item.details.title}</h4>
+          <p>{item.details.description}</p>
+        </div>
+      )
+    })
+  }
+
+  addTask() {
+    this.setState({
+      items: this.state.items.concat([
+        {
+          id: Math.floor(Math.random() * 100000),
+          details: { title: 'first title', description: 'first description' },
+        },
+      ]),
+    })
+  }
+
   render() {
     return (
       <div>
-        <div>Rest Details</div>
+        <div className="Input-Box">
+          <input name="title" />
+          <textarea name="description" />
+          <button onClick={this.addTask}>add</button>
+        </div>
+        <div>{this.listItems(this.state.items)}</div>
       </div>
     )
   }
